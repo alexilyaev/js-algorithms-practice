ac3e965f8cad49db13b4421c6390036db11ad90e
Alex Ilyaev <alexilyaev@gmail.com>
2017-09-18 15:43:19 +0300

    Added fib complexity

--- other/fibonacci.js
+++ other/fibonacci.js
@@ -2,11 +2,17 @@
 
 /**
  * Fibonacci sequence with recursion
+ * Time - O(2^n) - `fib` is called many times for some sub numbers O(2^n-2)
+ * Space - O(n) - How deep is the recursion tree, excluding constant multipliers
+ *
+ * Ref:
+ * Time - https://stackoverflow.com/questions/360748/computational-complexity-of-fibonacci-sequence/23095023#23095023
+ * Space - https://www.youtube.com/watch?v=dxyYP3BSdcQ
  */
 function fib(n) {
   // console.count('fib');
 
-  if (n === 0 || n === 1) {
+  if (n <= 1) {
     return n;
   }
 
@@ -19,23 +25,23 @@ console.log(fib(8));
 
 /**
  * Optimized to cache already computed results
+ * Time - O(n) - `fib` is called only once for each number
+ * Space - O(n) - Same as above, although the call stack is significantly smaller
+ *              - n for the cache Array and n for the call stack, so O(2n)
  */
-const cache = {};
+const cache = [];
 
 function fibOptimized(n) {
   // console.count('fibOptimized');
 
-  if (n === 0 || n === 1) {
+  if (n <= 1) {
     return n;
   }
 
-  const fibMinus1 = cache[n - 1] || fibOptimized(n - 1);
-  const fibMinus2 = cache[n - 2] || fibOptimized(n - 2);
+  cache[n - 1] = cache[n - 1] || fibOptimized(n - 1);
+  cache[n - 2] = cache[n - 2] || fibOptimized(n - 2);
 
-  cache[n - 1] = cache[n - 1] || fibMinus1;
-  cache[n - 2] = cache[n - 2] || fibMinus2;
-
-  return fibMinus1 + fibMinus2;
+  return cache[n - 1] + cache[n - 2];
 }
 
 console.log(fibOptimized(8));
